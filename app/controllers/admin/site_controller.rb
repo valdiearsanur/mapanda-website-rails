eb8ce78f7af8b1dc1c1be3f520045b3967aa0b9c
valdiearsanur <valdiearsanur@gmail.com>
2016-09-19 20:21:37 +0700

    First Commit

--- app/controllers/admin/site_controller.rb
+++ app/controllers/admin/site_controller.rb
@@ -1,4 +1,5 @@
 class Admin::SiteController < ApplicationController
+    before_action :set_admin_login, only: [:show, :destroy]
     layout 'admin'
 
     # GET /admin/login
@@ -8,7 +9,7 @@ class Admin::SiteController < ApplicationController
 
     # POST /admin/login_attemp
     def login_attemp
-        @admin_adminlogin = Admin::AdminLogin.new(admin_game_params)
+        @admin_adminlogin = Admin::AdminLogin.new(admin_login_params)
         respond_to do |format|
 
         end
@@ -22,12 +23,12 @@ class Admin::SiteController < ApplicationController
 
     private
         # Use callbacks to share common setup or constraints between actions.
-        def set_admin_game
+        def set_admin_login
             @admin_adminlogin = Admin::AdminLogin.find(params[:id])
         end
 
         # Never trust parameters from the scary internet, only allow the white list through.
-        def admin_game_params
+        def admin_login_params
             params.require(:admin_adminlogin).permit(:email, :password)
         end
 end
